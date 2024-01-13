fc13f2793d4feb077f6ada52f4e70cb4754844d3
Zach Nyman <zachery.nyman@gmail.com>
2024-01-12 17:21:25 -0700

    Completed project

--- src/com/skilldistillery/filmquery/app/FilmQueryApp.java
+++ src/com/skilldistillery/filmquery/app/FilmQueryApp.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 import com.skilldistillery.filmquery.database.DatabaseAccessor;
 import com.skilldistillery.filmquery.database.DatabaseAccessorObject;
+import com.skilldistillery.filmquery.entities.Actor;
 import com.skilldistillery.filmquery.entities.Film;
 
 public class FilmQueryApp {
@@ -12,8 +13,8 @@ public class FilmQueryApp {
 
 	public static void main(String[] args) {
 		FilmQueryApp app = new FilmQueryApp();
-		app.test();
-//		app.launch();
+//		app.test();
+		app.launch();
 	}
 
 	private void test() {
@@ -31,21 +32,27 @@ public class FilmQueryApp {
 
 	private void startUserInterface(Scanner keyboard) {
 		int userChoice;
+		Film film = null;
 		do {
 			System.out.println("Would you like to:");
 			System.out.println("'1' Look up a film by its ID");
 			System.out.println("'2' Look up a film by a keyword");
 			System.out.println("'3' Exit the application");
 			userChoice = keyboard.nextInt();
+			keyboard.nextLine();
 			
 			switch (userChoice) {
 			case 1:
 				System.out.println("What is the film ID you are looking for? Please enter a whole number.");
 				userChoice = keyboard.nextInt();
-				Film film = db.findFilmById(userChoice);
+				film = db.findFilmById(userChoice);
 				displayFilmData(film);
 				break;
 			case 2:
+				System.out.println("What is the keyword you would like to search films for?");
+				String keyword = keyboard.nextLine();
+				film = db.findFilmByKeyword(keyword);
+				displayFilmData(film);
 				break;
 			case 3:
 				System.out.println("Goodbye.");
@@ -59,7 +66,7 @@ public class FilmQueryApp {
 	
 	public void displayFilmData(Film film) {
 		if (film == null) {
-			System.out.println("I'm sorry, we couldn't find a film with that ID.");
+			System.out.println("I'm sorry, we couldn't find a film with that information.");
 		} else {
 			System.out.println("We have found your film, and here is its information:");
 			System.out.println("Title: " + film.getTitle());
@@ -67,6 +74,13 @@ public class FilmQueryApp {
 			System.out.println("Rating: " + film.getRating());
 			System.out.println("Description: " + film.getDescription());
 			System.out.println("Language: " + film.getLanguage());
+			System.out.println();
+			System.out.println("Starring:");
+			for (Actor actor : film.getActors()) {
+				System.out.println(actor.getFirstName() + " " + actor.getLastName());
+			}
+			
+			System.out.println();
 		}
 	}
 
